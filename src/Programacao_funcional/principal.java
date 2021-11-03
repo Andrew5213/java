f08e0d2f5e135d89adfcc534bc74cde6934e29c4
andrew <andrewoliveira282@gmail>
2021-11-03 16:32:32 -0300

    atualização da programação funcional

--- src/Programacao_funcional/principal.java
+++ src/Programacao_funcional/principal.java
@@ -2,6 +2,7 @@ package Programacao_funcional;
 
 import Programacao_funcional.Enditities.Product;
 import Programacao_funcional.util.UpperCaseName;
+import Services.ProductServices;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,10 @@ public class principal {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        List<String> names =list.stream().map(p-> p.getName().toUpperCase()).collect(Collectors.toList());
-        names.forEach(System.out::println);
+        //List<String> names =list.stream().map(p-> p.getName().toUpperCase()).collect(Collectors.toList());
+       // names.forEach(System.out::println);
+        ProductServices ps = new ProductServices();
+        double sum = ps.filteredSum(list,p->p.getName().charAt(0)=='T');
+        System.out.println("sum ="+ String.format("%.2f",sum));
     }
 }
